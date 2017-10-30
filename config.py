27f14b7fd747d05dc491bf5fb87bd612f13a4ef3
David Mwangi <njorogedavid13@gmail.com>
2017-10-30 11:33:54 +0300

    add config files

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+
+class Config:
+    '''
+    General configurations
+    '''
+
+class ProdConfig(Config):
+    '''
+    production configuration child class
+
+    args:
+        config: the parent class
+    '''
+    pass
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://chutha:chutha@localhost/watchlist_test'
+#
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    args:
+        config: the parent class
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://chutha:chutha@localhost/watchlist'
+
+    DEBUG = True
+
+config_options={
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
