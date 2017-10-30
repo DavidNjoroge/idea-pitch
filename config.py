f8cef0a297fb2b57b60731fd0909dd50ee6561aa
David Mwangi <njorogedavid13@gmail.com>
2017-10-30 11:44:25 +0300

    add manage.py wit development configurations

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class DevConfig(Config):
     args:
         config: the parent class
     '''
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://chutha:chutha@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://chutha:chutha@localhost/pitches'
 
     DEBUG = True
 
