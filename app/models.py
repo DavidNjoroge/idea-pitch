ba59848cd3872f33cb4cc78048f64329b1c0e866
David Mwangi <njorogedavid13@gmail.com>
2017-10-30 16:31:12 +0300

    make the server run :)

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from . import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__='users'
 
     id = db.Column(db.Integer,primary_key=True)
-    username=Column(db.String(255))
-    email=Column(db.String(255))
-    bio=Column(db.String(255))
-    pass_secure=Column(db.String(255))
+    username=db.Column(db.String(255))
+    email=db.Column(db.String(255))
+    bio=db.Column(db.String(255))
+    pass_secure=db.Column(db.String(255))
     pitches=db.relationship('Pitch',backref='user',lazy='dynamic')
 
 
@@ -40,3 +41,22 @@ class Pitch(db.Model):
     def get_pitches(cls,id):
         pitches=Pitch.query.filter_by(user_Id=id)
         return pitches
+
+@classmethod
+class Comment:
+    __tablename__='comments'
+
+    id=db.Column(db.Integer,primary_key=True)
+    body=db.Column(db.String(255))
+    time=db.Column(db.DateTime,default=datetime.utcnow)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments=Comment.query.filter_by(user_Id=id)
+        return comments
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Pitches,Comments
+from app.models import User,Pitch,Comment
 from flask_migrate import Migrate,MigrateCommand
 
 
@@ -13,7 +13,7 @@ manager.add_command('server',Server)
 @manager.shell
 def make_shell_context():
     return dict(app=app,db=db,User=User)
-@manager.MigrateCommand
+@manager.command
 def test():
     import unittest
 
